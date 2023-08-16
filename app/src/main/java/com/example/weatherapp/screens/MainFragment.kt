83c19cc7c2e609ce02a18d3be3d45dd27d99bffc
FaimiZufarov <faimi14102001@gmail.com>
2023-08-16 23:15:07 +0600

    Code Checking

--- app/src/main/java/com/example/weatherapp/screens/MainFragment.kt
+++ app/src/main/java/com/example/weatherapp/screens/MainFragment.kt
@@ -15,7 +15,14 @@ import com.example.weatherapp.network.Day
 import com.example.weatherapp.network.Forecast
 import com.example.weatherapp.network.Forecastday
 import com.example.weatherapp.network.Weather
+import com.example.weatherapp.network.WeatherApiService
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 class MainFragment : Fragment() {
 
@@ -34,10 +41,25 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.sendRequestButton.setOnClickListener {
-            viewModel.getWeather()
-            recyclerView = binding.cityRecyclerView
-            recyclerView.adapter = DaysAdapter(viewModel.weather.value!!)
+        val retrofit = Retrofit.Builder()
+            .addConverterFactory(MoshiConverterFactory.create(
+                Moshi.Builder()
+                    .add(KotlinJsonAdapterFactory())
+                    .build()
+            ))
+            .baseUrl("https://api.weatherapi.com/v1/")
+            .build()
+
+        CoroutineScope(Dispatchers.Main).launch {
+            val api = retrofit.create(WeatherApiService::class.java)
+            val model = api.getWeather(
+                "6c40f3b21c3642b4b8261922231308",
+                "Moscow",
+                "no",
+                "4",
+                "no"
+            )
+            binding.cityRecyclerView.adapter = DaysAdapter(model)
         }
 
     }
